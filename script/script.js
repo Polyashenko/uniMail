d010e3e9b96b75f4e0764ce90f2924b35313d663
Dmitry <polyashenko1@gmail.com>
2017-04-22 22:18:26 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -18,3 +18,21 @@ $(document).ready(function() {
 	});
 
 });
+
+
+	$("form").submit(function() { //Change Добавляем класс формы .callback
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change добавляем / (нужно добавить слеш, если брать с корня папку /mail.php)
+			data: th.serialize()
+		}).done(function() {
+			$(th).find('.success').addClass('active').css('display', 'flex').hide().fadeIn(); // клас active и дисплей флекс выровнять по центру блока
+			setTimeout(function() {
+				$(th).find('.success').removeClass('active').fadeOut(); // убераем клас active и прячем его
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
